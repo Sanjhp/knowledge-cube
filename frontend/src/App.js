151e83918cfc16f29eccdde7a96448a40761a627
Palak Agarwal <palak.agarwal2901@gmail.com>
2023-09-28 15:52:09 +0530

    herosection and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Apps from './Apps.module.css';
+import HeroSection from './components/HeroSection/HeroSection';
+import courses from './assets/courses.svg'
+import expert from './assets/expert.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className={Apps.container}>
+          <div className={Apps.cover}>
+            <div className={Apps.coverOverlay}></div>
+          <Navbar />
+          <HeroSection/>
+          </div>
+
+          <section>
+            <div className={Apps.grid}>
+              <div className={Apps.gridItem}>
+                <img src={courses} alt="cor" />
+                <h2>100k Online Courses</h2>
+
+              </div>
+              <div className={Apps.gridItem + ' ' + Apps.item2}>
+                <img src={expert} alt="cor" />
+                <h2>Expert Instruction</h2>
+              </div>
+              <div className={Apps.gridItem}>
+                <img src={courses} alt="cor" />
+                <h2>Lifetime Access</h2>
+              </div>
+            </div>
+          </section>
+        </div>
+      </Router>
+    </>
   );
 }
 
 export default App;
+// {/* <Main />
+//           <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/courses" element={<Courses />} />
+//           <Route path="/features" element={<Features />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/contact" element={<Contact />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/register" element={<Register />} />
+//         </Routes>
+//           <Footer /> */}
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './Navbar.module.css';
+import { Link, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import logo from '../../assets/navbar-logo.png'
+function Navbar() {
+  return (
+    <div>
+          <navbar className={styles.navbar}>
+              <div className={styles.logo}> 
+                  <img src={logo} alt="Logo" />
+                  <span>Knowledge Hub</span>
+              </div>
+              <nav className={styles.navigation}>
+                 
+                  <Link to="/login" className={styles.button}>Login</Link>
+                  <Link to="/register" className={styles.button}>Register</Link>
+              </nav>
+          </navbar>
+    </div>
+  )
+}
+
+export default Navbar;
