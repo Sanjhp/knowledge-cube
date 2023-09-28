3d5a8a14319312b9fd2bba676f1c646301f5f0e0
vaishali <vaishalirdj14@gmail.com>
2023-09-28 16:24:25 +0530

    upload course page building initiated

---
+++ frontend/src/pages/UploadCourse.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const UploadCourse = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-row">
+            <div className="flex flex-col">
+                <img src="https://i.ibb.co/g47QrX5/Screenshot-2023-09-27-at-11-27-20-PM-removebg-preview.png" 
+                className="w-64"/>
+            </div>
+        </div>
+        <div className="flex flex-row">
+
+        </div>
+
+    </div>
+  )
+}
+
+export default UploadCourse
