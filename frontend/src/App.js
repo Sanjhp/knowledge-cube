883dea0846be45f1195e4674fdb1263b065b60ec
vaishali <vaishalirdj14@gmail.com>
2023-09-29 19:41:26 +0530

    Course Collection page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter , Routes , Route} from "react-router-dom"
 import UploadCourse from './pages/UploadCourse';
+import CourseHeader from './pages/CourseHeader';
+import CourseCollection from './pages/CourseCollection';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<UploadCourse/>}></Route>
+      <Route path="/" element={<CourseCollection/>}></Route>
     </Routes>
     </BrowserRouter>
   );
