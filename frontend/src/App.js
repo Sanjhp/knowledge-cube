9bf716033ac407f98033738ca33ae288932d8c69
vaishali <vaishalirdj14@gmail.com>
2023-10-03 11:02:10 +0530

    course overview page complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-import UploadCourse from "./pages/UploadCourse";
-import CourseHeader from "./pages/CourseHeader";
-import CourseCollection from "./pages/CourseCollection";
+import UploadCourse from "./pages/UploadCourse/UploadCourse";
+import CourseHeader from "./pages/CourseCategories/CourseHeader";
+import CourseCollection from "./pages/CourseCategories/CourseCollection";
 import AllCourses from "./pages/AllCourses";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import Course from "./pages/UploadCourse/UploadCourse";
@@ -14,30 +14,33 @@ import ForgetPassword from "./pages/ForgetPassword/ForgetPassword";
 import ResetPassword from "./pages/ResetPassword/ResetPassword";
 import Signin from "./pages/Signin/Signin";
 import Signup from "./pages/Signup/Signup";
+import CourseOverview from "./pages/CourseOverview";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AllCourses />}></Route>
-      </Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/upload-course" element={<Course />} />
+        {/* <Route path="/" element={<AllCourses />}></Route> */}
+      
+      <Route exact path="/" element={<CourseOverview />} />
+      {/* <Route exact path="/upload-course" element={<Course />} />
       <Route exact path="/categories" element={<Categories />} />
       <Route exact path="/login" element={<Signin />} />
       <Route exact path="/register" element={<Signup />} />
       <Route exact path="/profile-update" element={<Updateprofile />} />
       <Route exact path="/forget-password" element={<ForgetPassword />} />
-      <Route exact path="/reset-password" element={<ResetPassword />} />
+      <Route exact path="/reset-password" element={<ResetPassword />} /> */}
+      </Routes>
       {/* <Route exact path="/collection" element={<Collection />} /> 
         <Route exact path="/courses" element={<Courses />} />
         <Route exact path="/features" element={<Features />} /> 
         <Route exact path="/about" element={<About />} />
         <Route exact path="/contact" element={<Contact />} />*/}
-      <AllCourses/>  
+      {/* <AllCourses/>  
       <UploadCourse />
       <CourseHeader />
-      <CourseCollection />
+      <CourseCollection /> */}
+
     </BrowserRouter>
   );
 }
