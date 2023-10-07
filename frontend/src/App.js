1e97233118cd7700358838eaefd0682cd66ffbca
Sandhya <sandhyaginare@gmail.com>
2023-10-07 13:39:11 +0530

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,29 +23,33 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Home />}></Route> */}
-        {/* <Route exact path="/upload-course" element={<Course />} />
+        <Route path="/" element={<Home />}></Route>
+        <Route exact path="/upload-course" element={<Course />} />
         <Route exact path="/categories" element={<CourseCategories />} />
+        <Route exact path="/all-courses" element={<AllCourses />} />
+        <Route exact path="/course-collection" element={<CourseCollection />} />
+        <Route exact path="/course-overview" element={<CourseOverview />} />
+        <Route exact path="/learner-dashboard" element={<LearnerDashboard />} />
+        <Route exact path="/creator-dashboard" element={<CreaterDashboard />} />
         <Route exact path="/login" element={<Signin />} />
         <Route exact path="/register" element={<Signup />} />
         <Route exact path="/profile-update" element={<Updateprofile />} />
         <Route exact path="/forget-password" element={<ForgetPassword />} />
-        <Route exact path="/reset-password" element={<ResetPassword />} /> */}
-
+        <Route exact path="/reset-password" element={<ResetPassword />} />
+        {/* <Route exact path="/course-header" element={ <CourseHeader /> } /> */}
+        {/* <Route exact path="/features" element={<Features />} />  */}
         {/* <Route exact path="/collection" element={<Collection />} />  */}
-        {/* <Route exact path="/courses" element={<CourseCollection />} />
-        <Route exact path="/features" element={<Features />} />  */}
+        {/* <Route exact path="/courses" element={<CourseCollection />} />   */}
         {/* <Route exact path="/about" element={<About />} /> */}
         {/* <Route exact path="/contact" element={<Contact />} /> */}
       </Routes>
-
-      <AllCourses/>  
+      {/* <AllCourses/>  
       <UploadCourse />
       <CourseHeader />
       <CourseCollection />
       <CourseOverview/>
       <LearnerDashboard/>
-      <CreaterDashboard/>
+      <CreaterDashboard/> */}
     </BrowserRouter>
   );
 }
