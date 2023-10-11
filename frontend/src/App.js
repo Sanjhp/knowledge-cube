755a3af04b523d288c5a6ccdb6bd82b9a75e7b37
Sandhya <sandhyaginare@gmail.com>
2023-10-11 08:05:32 +0530

    dynamic navigation to courses to overview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,23 +8,22 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import Course from "./pages/UploadCourse/UploadCourse";
-import Categories from "./pages/CourseCategories/CourseCategories";
 import Updateprofile from "./pages/UpdateProfile/Updateprofile";
 import ForgetPassword from "./pages/ForgetPassword/ForgetPassword";
 import ResetPassword from "./pages/ResetPassword/ResetPassword";
 import Signin from "./pages/Signin/Signin";
 import Signup from "./pages/Signup/Signup";
 import CourseOverview from "./pages/CourseOverview";
-import Features from "./components/Features/Features";
 import CourseCategories from "./pages/CourseCategories/CourseCategories";
 import LearnerDashboard from "./pages/LearnerDashboard";
 import CreaterDashboard from "./pages/CreaterDashboard";
-import Navbar from "./components/Navbar/Navbar";
-import CreatorNavbar from "./components/Navbar/CreatorNavbar";
-import LearnerNavbar from "./components/Navbar/LearnerNavbar";
+// import Navbar from "./components/Navbar/Navbar";
+// import CreatorNavbar from "./components/Navbar/CreatorNavbar";
+// import LearnerNavbar from "./components/Navbar/LearnerNavbar";
 import EditCourse from "./pages/EditCourse";
 import PrivateMessaging from "./pages/PrivateMessaging";
 import Chapter from "./pages/UploadCourse/UploadChapterByCourse";
+
 function App() {
   return (
     <BrowserRouter>
@@ -35,7 +34,7 @@ function App() {
         <Route exact path="/categories" element={<CourseCategories />} />
         <Route exact path="/all-courses" element={<AllCourses />} />
         <Route exact path="/course-collection" element={<CourseCollection />} />
-        <Route exact path="/course-overview" element={<CourseOverview />} />
+        <Route exact path="/course-overview/:courseId" element={<CourseOverview />} />
         <Route exact path="/learner-dashboard" element={<LearnerDashboard />} />
         <Route exact path="/creator-dashboard" element={<CreaterDashboard />} />
         <Route exact path="/login" element={<Signin />} />
